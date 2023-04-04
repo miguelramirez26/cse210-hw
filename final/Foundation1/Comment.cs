bc3bce88d233efb290cf514c4b0f034abab304f4
miguelramirez26 <26ramirez.m@gmail.com>
2023-04-04 23:05:45 +0200

    Foundation1 finished

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -2,15 +2,18 @@ using System;
 
 class Comment
 {
+    // Attributes
     private string _userName;
     private string _text;
 
+    // Constructors
     public Comment(string userName, string text)
     {
         _userName = userName;
         _text = text;
     }
 
+    // Methods
     public void DisplayComment()
     {
         Console.WriteLine($"{_userName}");
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -3,25 +3,31 @@ using System.Collections.Generic;
 
 class Video
 {
+    // Attributes
     private string _title;
     private string _author;
     private int _length;
     public List<Comment> _comments = new List<Comment>();
 
+    // Constructors
     public Video(string title, string author, int length)
     {
         _title = title;
         _author = author;
         _length = length;
     }
+
+    // Methods
     public void AddComment(Comment comment)
     {
         _comments.Add(comment);
     }
+
     public int NumOfComment()
     {
         return _comments.Count();
     }
+    
     public void DisplayVideo()
     {
         Console.Write($"{_title} - by {_author}\n");
